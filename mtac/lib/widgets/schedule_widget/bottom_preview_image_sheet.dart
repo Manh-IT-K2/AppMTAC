864f522063effbe6456b4dbb1bb4c01cf1ea0d44
Qmanh <quanmanh901@gmail.com>
2025-03-12 06:57:00 +0000

    update preview image and delete

--- mtac/lib/widgets/schedule_widget/bottom_preview_image_sheet.dart
+++ mtac/lib/widgets/schedule_widget/bottom_preview_image_sheet.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mtac/controllers/image_picker_controller.dart';
-
+import 'package:sizer/sizer.dart';
 
 class BottomPreviewImageSheet extends StatelessWidget {
   const BottomPreviewImageSheet({super.key});
@@ -12,22 +12,54 @@ class BottomPreviewImageSheet extends StatelessWidget {
 
     return Container(
       padding: const EdgeInsets.all(10),
-      height: MediaQuery.of(context).size.height * 0.7,
+      width: 100.w,
+      height: 70.h,
       child: Obx(() {
         return GridView.builder(
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-          ),
+              crossAxisCount: 2,
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              childAspectRatio: 0.5),
           itemCount: imageController.selectedImages.length,
           itemBuilder: (context, index) {
-            return ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.file(
-                imageController.selectedImages[index],
-                fit: BoxFit.cover,
-              ),
+            return Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.file(
+                    imageController.selectedImages[index],
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  top: 3.w,
+                  left: 3.w,
+                  child: GestureDetector(
+                    onTap: () {
+                      imageController.removeImage(index);
+                      if (imageController.selectedImages.isEmpty) {
+                        Navigator.pop(context);
+                      }
+                    },
+                    child: Container(
+                      height: 8.w,
+                      width: 8.w,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8.w),
+                      ),
+                      child: Icon(
+                        Icons.close,
+                        size: 4.w,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             );
           },
         );
