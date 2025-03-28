140c965249260c6607c5f0e588f1b6da23fc7275
Qmanh <quanmanh901@gmail.com>
2025-03-28 21:32:42 +0700

    update ui detailScheduleCollectionDriver

--- mtac/lib/views/driver/detail_schedule_collection_driver_screen.dart
+++ mtac/lib/views/driver/detail_schedule_collection_driver_screen.dart
@@ -33,52 +33,75 @@ class DetailScheduleCollectionDriverScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Thông tin chi tiết", style: PrimaryFont.headerTextBold(),),
+              Text(
+                "Thông tin chi tiết",
+                style: PrimaryFont.headerTextBold(),
+              ),
               Container(
-                padding: const EdgeInsets.all(16.0),
-                margin: EdgeInsets.only(top: 3.w),
+                margin: EdgeInsets.symmetric(vertical: 3.w),
                 width: 100.w,
-                height: 60.h,
+                //height: 60.h,
                 decoration: BoxDecoration(
                   color: Colors.grey.withOpacity(0.2),
                   borderRadius: BorderRadius.circular(5.w),
                 ),
-                child: Column(
+                child: const Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Tên công ty", style: PrimaryFont.bodyTextMedium(),),
-                    Text("name", style: PrimaryFont.titleTextBold(),),
-                    Text("Ngày thu gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("day", style: PrimaryFont.titleTextBold(),),
-                    Text("Thời gian thu gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("null", style: PrimaryFont.titleTextBold(),),
-                    Text("Biển số xe", style: PrimaryFont.bodyTextMedium(),),
-                    Text("number Plate", style: PrimaryFont.titleTextBold(),),
-                    Text("Địa chỉ thu gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("address", style: PrimaryFont.titleTextBold(),),
-                    Text("Ngày gửi lịch gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("senđaycollec", style: PrimaryFont.titleTextBold(),),
-                    Text("Số lượng nhận công", style: PrimaryFont.bodyTextMedium(),),
-                    Text("numberWorker", style: PrimaryFont.titleTextBold(),),
-                    Text("Khu vực vận chuyển", style: PrimaryFont.bodyTextMedium(),),
-                    Text("area", style: PrimaryFont.titleTextBold(),),
-                    Text("Loại hàng", style: PrimaryFont.bodyTextMedium(),),
-                    Text("typeGoods", style: PrimaryFont.titleTextBold(),),
-                    Text("Đơn giá vận chuyển", style: PrimaryFont.bodyTextMedium(),),
-                    Text("200000", style: PrimaryFont.titleTextBold(),),
-                    Text("Thông tin người liên hệ", style: PrimaryFont.bodyTextMedium(),),
-                    Text("Chị Giao", style: PrimaryFont.titleTextBold(),),
-                    Text("Trạng thái công nợ", style: PrimaryFont.bodyTextMedium(),),
-                    Text("chưa nghiệm thu", style: PrimaryFont.titleTextBold(),),
+                    _ItemInforDetail(
+                      title: "Tên công ty",
+                      subTitle: "Công ty TNHH Active Creation",
+                    ),
+                    _ItemRowInforDetail(
+                      title1: "Ngày thu gom",
+                      subTitle1: "06-01-2025",
+                      title2: "Biển số xe",
+                      subTitle2: "50H-04282",
+                    ),
+                    _ItemInforDetail(
+                      title: "Địa chỉ thu gom",
+                      subTitle:
+                          "359A, Ấp Long Bình, Xã Long Hiệp, Huyện Biên Lức, Tỉnh Long An",
+                    ),
+                    _ItemRowInforDetail(
+                      title1: "Thời gian thu gom",
+                      subTitle1: "null",
+                      title2: "Loại hàng",
+                      subTitle2: "Chất thải nguy hại",
+                    ),
+                    _ItemInforDetail(
+                      title: "Khu vực vận chuyển",
+                      subTitle: "Thủ Thừa(Long An) => Bình Tân(TP.HCM)",
+                    ),
+                    _ItemRowInforDetail(
+                      title1: "Ngày gửi lịch gom",
+                      subTitle1: "05-01-2025",
+                      title2: "Đơn giá vận chuyển",
+                      subTitle2: "2,400,000,00",
+                    ),
+                    _ItemInforDetail(
+                      title: "Số lượng nhân công",
+                      subTitle: "Nguyễn Văn A, Lê Văn B",
+                    ),
+                    _ItemRowInforDetail(
+                      title1: "Thông tin người liên hệ",
+                      subTitle1: "Chị Giao",
+                      title2: "Trạng thái công nợ",
+                      subTitle2: "Chưa nghiệm thu",
+                    ),
+                    SizedBox(height: 10,)
                   ],
                 ),
               ),
-              Text("Danh sách chi phí đi kèm", style: PrimaryFont.headerTextBold(),),
+              Text(
+                "Danh sách chi phí đi kèm",
+                style: PrimaryFont.headerTextBold(),
+              ),
               Container(
                 padding: const EdgeInsets.all(16.0),
                 margin: EdgeInsets.only(top: 3.w),
                 width: 100.w,
-                height: 60.h,
+                height: 30.h,
                 decoration: BoxDecoration(
                   color: Colors.grey.withOpacity(0.2),
                   borderRadius: BorderRadius.circular(5.w),
@@ -86,29 +109,30 @@ class DetailScheduleCollectionDriverScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("ID", style: PrimaryFont.bodyTextMedium(),),
-                    Text("121313", style: PrimaryFont.titleTextBold(),),
-                    Text("HẠNG MỤC", style: PrimaryFont.bodyTextMedium(),),
-                    Text("Thuê nhân công", style: PrimaryFont.titleTextBold(),),
-                    Text("ĐƠN GIÁ", style: PrimaryFont.bodyTextMedium(),),
-                    Text("280,000,00", style: PrimaryFont.titleTextBold(),),
-                    Text("SỐ LƯỢNG", style: PrimaryFont.bodyTextMedium(),),
-                    Text("2.00", style: PrimaryFont.titleTextBold(),),
-                    Text("THÀNH TIỀN", style: PrimaryFont.bodyTextMedium(),),
-                    Text("560,000,00", style: PrimaryFont.titleTextBold(),),
-                    Text("GHI CHÚ", style: PrimaryFont.bodyTextMedium(),),
-                    Text("Đi chung CN với Bibica", style: PrimaryFont.titleTextBold(),),
-                    Text("TRẠNG THÁI", style: PrimaryFont.bodyTextMedium(),),
-                    Text("Chưa nghiệm thu", style: PrimaryFont.titleTextBold(),),  
+                    Text(
+                      "ID",
+                      style: PrimaryFont.bodyTextMedium(),
+                    ),
+                    Text(
+                      "121313",
+                      style: PrimaryFont.titleTextBold(),
+                    ),
+                    Text(
+                      "HẠNG MỤC",
+                      style: PrimaryFont.bodyTextMedium(),
+                    ),
                   ],
                 ),
               ),
-              Text("Danh sách hành hoá", style: PrimaryFont.headerTextBold(),),
+              Text(
+                "Danh sách hành hoá",
+                style: PrimaryFont.headerTextBold(),
+              ),
               Container(
                 padding: const EdgeInsets.all(16.0),
                 margin: EdgeInsets.only(top: 3.w),
                 width: 100.w,
-                height: 60.h,
+                height: 30.h,
                 decoration: BoxDecoration(
                   color: Colors.grey.withOpacity(0.2),
                   borderRadius: BorderRadius.circular(5.w),
@@ -116,36 +140,154 @@ class DetailScheduleCollectionDriverScreen extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Tên công ty", style: PrimaryFont.bodyTextMedium(),),
-                    Text("name", style: PrimaryFont.titleTextBold(),),
-                    Text("Ngày thu gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("day", style: PrimaryFont.titleTextBold(),),
-                    Text("Thời gian thu gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("null", style: PrimaryFont.titleTextBold(),),
-                    Text("Biển số xe", style: PrimaryFont.bodyTextMedium(),),
-                    Text("number Plate", style: PrimaryFont.titleTextBold(),),
-                    Text("Địa chỉ thu gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("address", style: PrimaryFont.titleTextBold(),),
-                    Text("Ngày gửi lịch gom", style: PrimaryFont.bodyTextMedium(),),
-                    Text("senđaycollec", style: PrimaryFont.titleTextBold(),),
-                    Text("Số lượng nhận công", style: PrimaryFont.bodyTextMedium(),),
-                    Text("numberWorker", style: PrimaryFont.titleTextBold(),),
-                    Text("Khu vực vận chuyển", style: PrimaryFont.bodyTextMedium(),),
-                    Text("area", style: PrimaryFont.titleTextBold(),),
-                    Text("Loại hàng", style: PrimaryFont.bodyTextMedium(),),
-                    Text("typeGoods", style: PrimaryFont.titleTextBold(),),
-                    Text("Đơn giá vận chuyển", style: PrimaryFont.bodyTextMedium(),),
-                    Text("200000", style: PrimaryFont.titleTextBold(),),
-                    Text("Thông tin người liên hệ", style: PrimaryFont.bodyTextMedium(),),
-                    Text("Chị Giao", style: PrimaryFont.titleTextBold(),),
-                    Text("Trạng thái công nợ", style: PrimaryFont.bodyTextMedium(),),
-                    Text("chưa nghiệm thu", style: PrimaryFont.titleTextBold(),),
+                    Text(
+                      "Tên công ty",
+                      style: PrimaryFont.bodyTextMedium(),
+                    ),
+                    Text(
+                      "name",
+                      style: PrimaryFont.titleTextBold(),
+                    ),
+                    Text(
+                      "Ngày thu gom",
+                      style: PrimaryFont.bodyTextMedium(),
+                    ),
+                    Text(
+                      "day",
+                      style: PrimaryFont.titleTextBold(),
+                    ),
                   ],
                 ),
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _ItemInforDetail extends StatelessWidget {
+  const _ItemInforDetail({
+    super.key,
+    required this.title,
+    required this.subTitle,
+  });
+  final String title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      padding: EdgeInsets.symmetric(horizontal: 3.w),
+      width: 100.w,
+      height: 14.w,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(3.w),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            offset: const Offset(4, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: PrimaryFont.bodyTextMedium(),
+          ),
+          Text(
+            subTitle,
+            style: PrimaryFont.bodyTextBold(),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ItemRowInforDetail extends StatelessWidget {
+  const _ItemRowInforDetail({
+    super.key,
+    required this.title1,
+    required this.title2,
+    required this.subTitle1,
+    required this.subTitle2,
+  });
+  final String title1, title2, subTitle1, subTitle2;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      width: 100.w,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 3.w),
+            width: 42.w,
+            height: 12.w,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(3.w),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  offset: const Offset(4, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title1,
+                  style: PrimaryFont.bodyTextMedium(),
+                ),
+                Text(
+                  subTitle1,
+                  style: PrimaryFont.bodyTextBold(),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 3.w),
+            width: 42.w,
+            height: 12.w,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(3.w),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2),
+                  offset: const Offset(4, 4),
+                ),
+              ],
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title2,
+                  style: PrimaryFont.bodyTextMedium(),
+                ),
+                Text(
+                  subTitle2,
+                  style: PrimaryFont.bodyTextBold(),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
