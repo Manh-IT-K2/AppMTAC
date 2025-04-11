e9c2aa9509f3b320d75fef6a67dc099ec13a43d4
Qmanh <quanmanh901@gmail.com>
2025-04-11 17:29:48 +0700

    finish menu remote main

---
+++ mtac/lib/widgets/schedule_widget/curved_line.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CurvedLinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = Colors.grey
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
+
+    final path = Path();
+    path.moveTo(0, size.height); // bắt đầu từ trái, đáy
+    path.quadraticBezierTo(
+      0, size.width / 2, // điểm cong lên ở giữa
+      size.width, size.height, // kết thúc bên phải, đáy
+    );
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
